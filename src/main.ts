e8c2522375d15eb7ef2f0bc9817995a53816be2f
Debashish Patra <suvam0451@outlook.com>
2021-01-11 20:13:26 +0530

    Fix(refactor): Updated script to no longer consider input filepath in cloud filepath

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,19 @@ const dbx = new Dropbox({accessToken: process.env.DROPBOX_ACCESS_TOKEN});
 
 console.log(process.argv[2])
 
-fs.readFile(path.join(__dirname, process.argv[2]), (err, contents) => {
+fs.readFile(process.argv[2], (err, contents) => {
     if (err) {
         console.log('Error: ', err);
     }
 
     // This uploads basic.js to the root of your dropbox
-    dbx.filesUpload({path: "/" + process.argv[2], contents})
+    dbx.filesUpload({path: "/" + path.basename(process.argv[2]), contents})
         .then((response: any) => {
+            console.log("Upload succeeded")
             console.log(response);
         })
         .catch((uploadErr: Error<files.UploadError>) => {
+            console.log("Error occured during upload")
             console.log(uploadErr);
         });
 });
